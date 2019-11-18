41a43c9ed6c19e09e4ca35b61bfc0eb04394626a
MartinW <martin@tinyshinyapps.co.uk>
2019-11-18 19:03:15 +0000

    Pinned SwiftyBeaver version for Swift 4.2

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "1.6.0")
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .exact("1.6.0"))
     ],
     targets: [
         .target(name: "SwiftyBeaverVapor", dependencies: ["Vapor", "SwiftyBeaver"])
